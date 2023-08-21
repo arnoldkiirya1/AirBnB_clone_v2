3bba124cd6aa888fd4dc7a1aacd1c50cce98363a
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-21 15:43:33 +0300

    repo set up

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# module that defines State class
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
