7d6bc6a3c808ee63f0bd0eec5689311e0af88637
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-23 00:57:32 +0300

    unittests

--- tests/test_models/test_engines/test_db_storage.py
+++ tests/test_models/test_engines/test_db_storage.py
@@ -1,86 +1,49 @@
 #!/usr/bin/python3
-# test for db_storage
+# tests db
 
 import unittest
-from models.engine.db_storage import DBStorage
+import MySQLdb
 from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
+from models.engine.db_storage import DBStorage
 
-class TestDBStorage(unittest.TestCase):
-    def setUp(self):
-        """Set up the test environment"""
-        self.db = DBStorage()
-        self.db.reload()
-        self.session = self.db._DBStorage__session
+class TestDBStorageWithMySQL(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Connect to the test MySQL database
+        cls.db = MySQLdb.connect(host="localhost", user="testuser", passwd="testpass", db="testdb")
+        cls.cursor = cls.db.cursor()
 
-    def tearDown(self):
-        """Clean up after each test"""
-        self.db.close()
+        # Set up DBStorage and create tables
+        cls.storage = DBStorage()
+        cls.storage.reload()
 
-    def test_create_and_retrieve_objects(self):
-        """Test creating and retrieving objects from the database"""
-        state = State(name="California")
-        city = City(name="Los Angeles", state_id=state.id)
-        user = User(email="test@example.com", password="password")
-        
-        # Add objects to session and save to the database
-        self.session.add(state)
-        self.session.add(city)
-        self.session.add(user)
-        self.session.commit()
+    @classmethod
+    def tearDownClass(cls):
+        # Close the database connection
+        cls.db.close()
 
-        # Retrieve objects using all() method
-        retrieved_state = self.db.all(State).get("State." + state.id)
-        retrieved_city = self.db.all(City).get("City." + city.id)
-        retrieved_user = self.db.all(User).get("User." + user.id)
+    def test_add_state_to_db(self):
+        # Get initial count of records in the states table
+        initial_count = self._get_states_count()
 
-        self.assertEqual(retrieved_state, state)
-        self.assertEqual(retrieved_city, city)
-        self.assertEqual(retrieved_user, user)
+        # Create a new State object
+        new_state = State(name="California")
 
-    def test_update_and_delete_objects(self):
-        """Test updating and deleting objects from the database"""
-        user = User(email="test@example.com", password="password")
-        self.session.add(user)
-        self.session.commit()
+        # Add the State object to the session and commit to the database
+        self.storage.new(new_state)
+        self.storage.save()
 
-        # Update user object
-        user.email = "updated@example.com"
-        self.session.commit()
+        # Get count of records in the states table after adding the new State
+        final_count = self._get_states_count()
 
-        # Retrieve updated user
-        retrieved_user = self.db.all(User).get("User." + user.id)
-        self.assertEqual(retrieved_user.email, "updated@example.com")
+        # Check if the count increased by 1 after adding the new State
+        self.assertEqual(final_count, initial_count + 1)
 
-        # Delete user
-        self.session.delete(user)
-        self.session.commit()
+    def _get_states_count(self):
+        # Execute SQL query to get the count of records in the states table
+        self.cursor.execute("SELECT COUNT(*) FROM states")
+        count = self.cursor.fetchone()[0]
+        return count
 
-        # Ensure user is not in the database
-        retrieved_user = self.db.all(User).get("User." + user.id)
-        self.assertIsNone(retrieved_user)
-
-    def test_table_creation(self):
-        """Test if tables are created properly"""
-        # Ensure State table is created
-        self.assertTrue(State.__tablename__ in self.db.__engine.table_names())
-
-    def test_session_management(self):
-        """Test if sessions are managed correctly"""
-        user = User(email="test@example.com", password="password")
-        self.session.add(user)
-        self.session.commit()
-
-        # Retrieve user in a new session
-        new_session = self.db._DBStorage__session_factory()
-        retrieved_user = new_session.query(User).filter_by(email="test@example.com").first()
-        new_session.close()
-
-        self.assertEqual(retrieved_user.email, "test@example.com")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
