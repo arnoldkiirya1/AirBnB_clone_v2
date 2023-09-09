531111e8d74dae1d9ea9956238f0b8080118220e
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-09-09 16:42:10 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,56 +1,91 @@
 #!/usr/bin/python3
-"""Fabric script to clean up outdated archives"""
-from fabric.api import local, env, run
-from datetime import datetime
+'''deletes out-of-date archives, using the function do_clean'''
 import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
-env.hosts = ['<54.237.67.184 web-01>', '<54.165.90.9 web-02>']
-env.user = 'ubuntu' 
-env.key_filename = '~/.ssh/id_rsa' 
+
+env.hosts = ['54.237.67.184', '54.165.90.9']
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
 
 
 def do_clean(number=0):
-    """Deletes out-of-date archives."""
-    try:
-        number = int(number)
-    except ValueError:
-        return
-
-    if number < 0:
-        return
-
-    if number == 0 or number == 1:
-        number = 1
+    """Deletes out-of-date archives of the static files.
+    Args:
+        number (Any): The number of archives to keep.
+    """
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
     else:
-        number += 1
-
-    # Delete outdated archives in the versions folder
-    with lcd("versions"):
-        local("ls -t | tail -n +{} | xargs -I {{}} rm -f {{}}".format(number))
-
-    # Delete outdated archives in the web_static/releases folder
-    with cd("/data/web_static/releases"):
-        run("ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number))
-
-
-def do_clean_remote(number=0):
-    """Deletes out-of-date archives on remote servers."""
-    try:
-        number = int(number)
-    except ValueError:
-        return
-
-    if number < 0:
-        return
-
-    if number == 0 or number == 1:
-        number = 1
-    else:
-        number += 1
-
-    # Delete outdated archives in the versions folder on remote servers
-    run("cd ~/AirBnB_clone_v2/versions && ls -t | tail -n +{} | xargs -I {{}} rm -f {{}}".format(number))
-
-    # Delete outdated archives in the web_static/releases folder on remote servers
-    run("cd /data/web_static/releases && ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number))
-
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
